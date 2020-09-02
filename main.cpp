b077d420a6b868acb210762a2fae2dda390c1232
Unekwu <39774708+Unekwu@users.noreply.github.com>
2020-09-02 01:07:32 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,34 +14,27 @@ class Key
   std::string second;
   int         third;
 
-//constructor
   Key (std::string f, std::string s, int t): first(f), second(s), third(t){
   }
-  //comparator functor that would check if the two objects are the same
-  //used in the unordered map 
   bool operator==(const Key &other) const
   { 
   return (first == other.first
             && second == other.second);
           //  && third == other.third);
   }
-//friend function that can access private members, and this overloads the << operator to be used for classes
-//this block prints out the output of the unordered map to console
+  
  friend std::ostream& operator<< (std::ostream& out, const Key& rhs) {
         out << rhs.first<<" : ";
         out <<rhs.second<<" : ";
     
     }
-
-//this block prints out the output of the unordered map to the files
+  
 friend std::ofstream& operator<< (std::ofstream& fout, const Key& rhs) {
         fout<<rhs.first<<" : ";
         fout<<rhs.second<<" : ";
     }
-
 };
 
-//defining the hash values of the username and password that would be used as key to the unordered map
 namespace std {
 template <>
 struct hash<Key>
@@ -59,51 +52,45 @@ struct hash<Key>
 
 int main()
 {
-  std::ofstream fout;// output stream used to write to a file 
-  std::string userName;//string to store the username 
-  std::string passWord;// string to store password
-  int id;//string to store ID
-  int trial = 0; //string to store number of trials 
-  int condition = 1;//string to store case condition 
-  int choice;//string to store the choice on the menu
-
-//unordered map used to store the student info
-  std::unordered_map<Key,int> setOfStud; 
-  //do while loop to get the options to repeat until conditon to terminate is reached
+  std::ofstream fout;
+  std::string userName;
+  std::string passWord;
+  int id;
+  int trial = 0; 
+  int condition = 1;
+  int choice;
 
 
-do{
-std::cout<<"Enter an option"<<std::endl;
-std::cout<<"1. Exit"<<std::endl;
-std::cout<<"2. Login by inserting username, password "<<std::endl;
-std::cin>>choice;//enter the choice from menu
-std::cout<<std::endl;
+    std::unordered_map<Key,int> setOfStud; 
+  do{
+  std::cout<<"Enter an option"<<std::endl;
+  std::cout<<"1. Exit"<<std::endl;
+  std::cout<<"2. Login by inserting username, password "<<std::endl;
+  std::cin>>choice;
+  std::cout<<std::endl;
 
 
 
 switch(choice){
 case 1:
     std::cout<<"Exiting"<<std::endl;
-    condition = 0;//condition to exit the loop
+    condition = 0;
     break;
 case 2:
   std::cout<<"Enter the name you want to enter"<<std::endl;
-  std::cin>>userName;//enter user name 
+  std::cin>>userName;
   std::cout<<"Enter the password you want to enter"<<std::endl;
-  std::cin>>passWord;//enter passowrd
-  srand(time(NULL));//seed to generate random values 
-  id = rand()%100+100000; //random numbers generated for the students 
- 
-    //res is used to store the values of the insert operation
+  std::cin>>passWord;
+  srand(time(NULL));
+  id = rand()%100+100000;
+    
      auto res= setOfStud.insert(std::make_pair(Key(userName,passWord,id),id));
      if(res.second == false){
      while (trial<5){
       std::cout << "Failed to insert with duplicate name - "<<userName << std::endl;
-      //write the breach to file 
       fout.open("attemptedduplicate.txt");
       fout<< "Name" << " :: " <<"Password" << " :: " << "ID" <<std::endl;
       fout<< userName << " :: " <<passWord << " :: " << id <<std::endl;
-    //write the breach to console
       std::cout<<"unauthorized access...Try again "<<std::endl;
       std::cout<<"Enter the name you want to enter again "<<std::endl;
       std::cin>>userName;
@@ -123,15 +110,12 @@ case 2:
   }
   
 } while(condition);
-//open authenticate .txt to write the output of the inserted users
  fout.open("authenticate.txt");
  if(fout.is_open()){
- for (auto& x : setOfStud) {
-      //display to file 
+ for (auto& x : setOfStud) { 
         fout << x.first << ": " << x.second << std::endl;
-        //display to console
         std::cout << x.first << ": " << x.second << std::endl;
     }
-    fout.close();//close the file 
+    fout.close(); 
  }
 }
